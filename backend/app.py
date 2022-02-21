c87b5baff03a88b14a64fbbed952826b646c8983
unknown <zahidrangwala3@gmail.com>
2022-02-20 23:07:48 -0500

    added time endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+import time
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
